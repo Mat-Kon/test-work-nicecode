4e5bba0a376de4c73dd2f4568287ef63d95e492f
Mat-Kon <mat_kon@mail.ru>
2024-05-04 18:00:56 +0300

    feat: add types for user

---
+++ src/_types/types.ts
@@ -0,0 +1,40 @@
+interface User {
+  id: string;
+  name: string;
+  avatar: string | null;
+  notes: Note[];
+  consultations: Consultation[];
+  videos: Video[];
+  events: UserEvent[];
+}
+
+interface Note {
+  id: string;
+  content: string;
+  createDate: string;
+  images: string[];
+}
+
+interface Consultation {
+  id: string;
+  category: 'online' | 'offline';
+  start: string;
+  end: string;
+  status: 'active' | 'confirmed' | 'not confirmed' | 'done';
+}
+
+interface Video {
+  url: string;
+  cover: string | null;
+  title: string;
+  author: string;
+  start: string;
+  end: string;
+}
+
+interface UserEvent {
+  id: string;
+  title: string;
+  cover: string | null;
+  category: 'Вебинар';
+}
