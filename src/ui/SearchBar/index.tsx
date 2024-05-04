4e59cfedd9398aaba9d00297519d8e2566faf445
Mat-Kon <mat_kon@mail.ru>
2024-05-04 12:25:30 +0300

    feat: create sort btn and add conntact btn

--- src/ui/SearchBar/index.tsx
+++ src/ui/SearchBar/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from './style.module.scss';
 import SearchBtn from "../buttons/SearchBtn";
+import SortBtn from "../buttons/SortBtn";
+import AddContactBtn from "../buttons/AddContactBtn";
 
 const SearchBar: React.FC = () => {
   return (
     <div style={{ display: 'flex', alignItems: 'center', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)', padding: '10px', borderRadius: '8px' }}>
       <SearchBtn />
       <input type="text" placeholder="Поиск" style={{ flex: 1, border: 'none', outline: 'none' }}/>
-      <button className={styles.sortBtn}></button>
-      <button className={styles.addBtn}></button>
+      <SortBtn />
+      <AddContactBtn />
     </div>
   );
 };
---
+++ src/ui/buttons/AddContactBtn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const AddContactBtn: React.FC = () => {
+  return (
+    <button className={styles.addContactBtn}></button>
+  );
+};
+
+export default AddContactBtn;
