e78824929664e1c196dd57536e1628f5c09c24c5
Mat-Kon <mat_kon@mail.ru>
2024-05-04 12:15:25 +0300

    feat: create search btn

--- src/ui/SearchBar/index.tsx
+++ src/ui/SearchBar/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './style.module.scss';
+import SearchBtn from "../buttons/SearchBtn";
 
 const SearchBar: React.FC = () => {
   return (
     <div style={{ display: 'flex', alignItems: 'center', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)', padding: '10px', borderRadius: '8px' }}>
-      <button className={styles.searchBtn}></button>
+      <SearchBtn />
       <input type="text" placeholder="Поиск" style={{ flex: 1, border: 'none', outline: 'none' }}/>
       <button className={styles.sortBtn}></button>
       <button className={styles.addBtn}></button>
---
+++ src/ui/buttons/SearchBtn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const SearchBtn: React.FC = () => {
+  return (
+    <button className={styles.searchBtn}></button>
+  );
+};
+
+export default SearchBtn;
